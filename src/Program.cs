c0b1374573f6ee0ba74e18bee042a6206d7e81e1
Danny-1720 <dannyonrun@gmail.com>
2025-05-09 10:09:29 +0530

    Program.cs

---
+++ src/Program.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Threading;
+
+class Program
+{
+    static void Main()
+    {
+        while (true)
+        {
+            Console.WriteLine("NudgeEngine is running...");
+            Thread.Sleep(5000);
+        }
+    }
+}
